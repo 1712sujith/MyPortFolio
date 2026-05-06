1bfd0cc9423f6e9c007170877c2643910886c58e
Sujith Kumar <sujithkumar12172002@gmail.com>
2026-05-06 10:57:19 +0530

    second commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,6 +9,7 @@ const Cart = () => {
         {cart.length>0?(cart.map((item)=><p key={item.id}>{item.name}
             <img class=" h-60 w-60" src={item.img}  alt="" /> <p className='text-center'>$45.0</p>
           </p> )):( <p>Cart is empty</p>)}
+          
     </div>
   )
 }
